868ae483d38d939ec3e24e33b616143a34dad001
zpr1g <zpr1g@users.noreply.github.com>
2020-08-22 14:58:33 +0000

    deploy: 7882dd6b5f31235a0e0d3b1a729331f74028050c

---
+++ precache-manifest.4b947e95fef058f0c1305b3f4b03dd07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e8ff0a8cbd7083fc52819d67662760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "273fa357c29f4d49c0d4",
+    "url": "/static/css/main.30d8fb4d.chunk.css"
+  },
+  {
+    "revision": "7ba187da40f47a2d9695",
+    "url": "/static/js/2.776c2b53.chunk.js"
+  },
+  {
+    "revision": "5ebc418b151f0d1d078277eb120a117e",
+    "url": "/static/js/2.776c2b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273fa357c29f4d49c0d4",
+    "url": "/static/js/main.023106f9.chunk.js"
+  },
+  {
+    "revision": "cac7fc5cdf9b29d594bd",
+    "url": "/static/js/runtime-main.4d600288.js"
+  }
+]);
