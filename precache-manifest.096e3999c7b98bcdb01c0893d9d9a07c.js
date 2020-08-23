79d8bdfdc4c3dbbfd4b77e79ae19813ca8884222
zpr1g <zpr1g@users.noreply.github.com>
2020-08-23 03:23:32 +0000

    deploy: 378ab831813f94c64ed0d6a017ad3f6642ec44c4

--- precache-manifest.096e3999c7b98bcdb01c0893d9d9a07c.js
+++ precache-manifest.096e3999c7b98bcdb01c0893d9d9a07c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e7ca152eb16a5ea28b6432c717359c22",
+    "revision": "e4648bb36a6311d1f41069f870a2a9cb",
     "url": "/logo-generator/index.html"
   },
   {
-    "revision": "3f6699ca76eaec750ba1",
+    "revision": "c717f1c8bacbf642a205",
     "url": "/logo-generator/static/css/main.30d8fb4d.chunk.css"
   },
   {
-    "revision": "9dcceceae0cfce7ffafd",
-    "url": "/logo-generator/static/js/2.7b130a9f.chunk.js"
+    "revision": "82fced4dae6d1fea3be6",
+    "url": "/logo-generator/static/js/2.305533c5.chunk.js"
   },
   {
     "revision": "5ebc418b151f0d1d078277eb120a117e",
-    "url": "/logo-generator/static/js/2.7b130a9f.chunk.js.LICENSE.txt"
+    "url": "/logo-generator/static/js/2.305533c5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3f6699ca76eaec750ba1",
-    "url": "/logo-generator/static/js/main.ba33416f.chunk.js"
+    "revision": "c717f1c8bacbf642a205",
+    "url": "/logo-generator/static/js/main.47eeb45d.chunk.js"
   },
   {
     "revision": "aab3cec2d50be5be5812",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/logo-generator/precache-manifest.c51b27d6415360ebe3573afe57b65255.js"
+  "/logo-generator/precache-manifest.096e3999c7b98bcdb01c0893d9d9a07c.js"
 );
 
 self.addEventListener('message', (event) => {
